80d08a60197f1bd05509d21730617f8888468fe8
rkadlick <27776470+rkadlick@users.noreply.github.com>
2025-05-21 19:48:33 -0700

    init: flip vercel.json builds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import MainContent from "@/components/MainContent";
 
+console.log(
+  'SANITY:',
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION
+)
 export default function Home() {
   return <MainContent />;
 }
